75ef5a2e178d415d0e033acf6507c99f134448d1
iamisafi <isafi@gmail.com>
2019-08-19 12:59:37 +0600

    add functional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Greet'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Now i am enter in React Js world.</h1>
-        </header>
-        <p className="App-intro">
-          It's my first React js App waoo.
-        </p>
-      </div>
+         <Greet />
+     </div>
     );
   }
 }
