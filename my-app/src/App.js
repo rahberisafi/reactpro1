d52f0291f773ae1b9f8ac84639847d4f9f231e2c
iamisafi <isafi@gmail.com>
2019-08-25 11:35:30 +0600

    add props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,9 +9,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-         {/* <Greet */}
+      <Greet name="Bruce" heroName="Batman">
+      <p>This is children props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Superman">
+      <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="Wonder Woman" />
+      <Welcome name="Bruce" heroName="Batman" />
+      <Welcome name="Clark" heroName="Superman" />
+      <Welcome name="Diana" heroName="Wonder Women" />
+
+
+         {/* <Hello /> */}
          {/* <Welcome /> */}
-         <Hello />
+
      </div>
     );
   }
--- my-app/src/components/Greet.js
+++ my-app/src/components/Greet.js
@@ -6,6 +6,17 @@ import React from 'react'
 
 // export const Greet = () => <h1>Hello Isafi</h1>
 
-const Greet = () => <h1>Hello Isafi</h1>
+const Greet = props => {
+  console.log(props)
+  return (
+    <div>
+    <h1>
+    Hello {props.name} a.k.a {props.heroName}
+    </h1>
+    {props.children}
+    </div>
+  )
+
+}
 
 export default Greet
